e6d325721c23b020fa4252ad8fcf0c6261675615
stark5200 <a.taha5200@gmail.com>
2023-10-22 22:22:23 +0200

    Custom Portfolio using Astro

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import preact from "@astrojs/preact";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), preact(), tailwind()]
+});
