676e35bf4d6db979db8d745fae1c30ad947fc3fe
stark5200 <a.taha5200@gmail.com>
2023-12-18 04:42:35 +0100

    let me check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import preact from "@astrojs/preact";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), preact(), tailwind()]
+  integrations: [react(), preact(), tailwind()],
+  output: 'server',
+  adapter: vercel(),
 });
